b9842e3026794013dbf5d325ea6c22ce510558e4
kongou <oukong84@gmail.com>
2015-07-24 09:30:05 +0700

    Link Review to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723174143) do
+ActiveRecord::Schema.define(version: 20150724020955) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150723174143) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
